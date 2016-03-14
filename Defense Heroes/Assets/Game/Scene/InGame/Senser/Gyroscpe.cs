43515f11d91455c281306912ea17e2629ca5907d
gunsct <gunsct@gmail.com>
2016-03-14 22:27:50 +0900

    자이로 센서 카메라 앵글 고정은 함
    위쪽이 살짝 이상하긴함 가속도은 뺌..
    이제 progress bar 연동하고 본격 유닛생산 및 길찾기 연동

--- Defense Heroes/Assets/Game/Scene/InGame/Senser/Gyroscpe.cs
+++ Defense Heroes/Assets/Game/Scene/InGame/Senser/Gyroscpe.cs
@@ -22,7 +22,7 @@ public class Gyroscpe : MonoBehaviour {
 		// 기본 자이로는 오브젝트 관찰 기준이라 카메라 기준으로 변경
 		//쿼터니언 하나 만들고
 		Quaternion transquat = Quaternion.identity; 
-		transquat.w = gyro.attitude.w; 
+		transquat.w = gyro.attitude.w;
 
 		//x, y축의 값을 반대로 뒤집음
 		transquat.x = -gyro.attitude.x; 
@@ -31,6 +31,12 @@ public class Gyroscpe : MonoBehaviour {
 
 		// 변경된 쿼터니언을 안드로이드 자이로 기본 축 수정과 함께 카메라에 적용. 스크립트는 카메라에.
 		transform.rotation = Quaternion.Euler (90, 0, 0) * transquat;
+
+		//각도제한 그런데 자이로가 90-360-270-360 구조로 되어있음
+		if (90.0f > transform.localEulerAngles.x && transform.localEulerAngles.x > 60.0f)
+			transform.eulerAngles = new Vector3 (60.0f, transform.localEulerAngles.y, transform.localEulerAngles.z);
+
+		if (270.0f < transform.localEulerAngles.x && transform.localEulerAngles.x < 240.0f)
+			transform.eulerAngles = new Vector3 (240.0f, transform.localEulerAngles.y, transform.localEulerAngles.z);
 	}
 }
-//http://quest3dkorea.com/m/11049242 참고
