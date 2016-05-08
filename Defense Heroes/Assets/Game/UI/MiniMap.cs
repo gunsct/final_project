f910b6981883e9b301019bd10bf1ead0ee42f7e5
gunsct <gunsct@gmail.com>
2016-05-08 23:19:56 +0900

    미니맵 구현완료
    렌더 텍스쳐로 카메라 랜더링하고 지유아이 텍스쳐로 적이랑 본인 오브젝트에 달아서 보이게 해줌 유아이면에서 좀더 가다듬을 필요가
    느껴지긴함

---
+++ Defense Heroes/Assets/Game/UI/MiniMap.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiniMap : MonoBehaviour {
+	public GUITexture redRect;
+	private Camera miniCam;
+	private Vector3 viewPortPos;
+
+	// Use this for initialization
+	void Start() {
+		miniCam = (Camera)GameObject.Find ("MiniMap Camera").GetComponent<Camera>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		viewPortPos = miniCam.WorldToViewportPoint(this.transform.position);
+		redRect.transform.position = new Vector2(viewPortPos.x -0.5f, viewPortPos.y - 0.5f);
+	}
+}
